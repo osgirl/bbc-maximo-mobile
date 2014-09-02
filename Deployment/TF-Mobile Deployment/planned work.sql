df9839945b8ce46d33be6ad52cfcb7d91ea1e2d4
TIMMINSA <andrew.timmins@interservefm.com>
2014-09-02 10:21:01 +0100

    Train Properties

--- Deployment/TF-Mobile Deployment/planned work.sql
+++ Deployment/TF-Mobile Deployment/planned work.sql
@@ -1,7 +1,5 @@
 wonum in (Select top 10 wonum from workorder 
-Where ( (datediff(dd,0,sneconstraint) <= datediff(dd,0,dateadd(day,7,getdate())))
-      Or (datediff(dd,0,schedstart) <= datediff(dd,0,dateadd(day,7,getdate())))
-      )
-And Status in ('DISPATCH','TRAVEL','ONSITE','START','ONHOLD','WOCOMP')
+Where 
+ Status in ('DISPATCH','TRAVEL','ONSITE','START','ONHOLD','WOCOMP')
 And woclass='WORKORDER' and worktype='PPM'
-And wonum in (Select wonum from assignment where laborcode in (Select laborcode from labor where status = 'ASSIGNED' and personid=:personid and IRV_CONTMETHOD='MOBILE')))
+And wonum in (Select wonum from assignment where laborcode in (Select laborcode from labor where    IRV_CONTMETHOD='MOBILE' and personid=:personid ) and status = 'ASSIGNED'))
